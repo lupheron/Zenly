8afe298f425f89010cff7f1c4fbe30fa4de49352
raze. <over404lord@gmail.com>
2025-05-30 20:47:27 +0500

    Color

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                mulish: ['Mulish-Regular', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
