c6318fbba6d7d87c322a27c8e3ef4d61759aa52c
raze. <over404lord@gmail.com>
2025-06-26 17:19:00 +0500

    Back button

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,4 +1,7 @@
+"use client"
 import Aside from '@/src/components/Aside/Aside'
+import ButtonDefault from '@/src/components/Button/ButtonDefault'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { ReactNode } from 'react'
 
 export default function UserLayout({ children }: { children: ReactNode }) {
@@ -6,6 +9,14 @@ export default function UserLayout({ children }: { children: ReactNode }) {
         <div className="flex min-h-screen">
             <Aside />
             <div className='w-full height-full p-30 flex-1 overflow-auto'>
+                <div className='cursor-pointer' onClick={() => window.history.back()}>
+                    <ArrowBackIcon />
+                    <ButtonDefault
+                        label='Qaytish'
+                        onClick={() => console.log('Qaytish tugmasi bosildi')}
+                        customClasses='bg-transparent !text-black tracking-[1px] text-xl mb-5 hover:bg-transparent !px-0 !py-0 ml-2'
+                    />
+                </div>
                 <main className="h-full border-1 rounded-2xl bg-light-gray border-gray-200 shadow-[4px_0_6px_-1px_rgba(0,0,0,0.1)] p-10">{children}</main>
             </div>
         </div>
