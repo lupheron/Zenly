b7f80d744243f77f75833bb363f8a3dafa3c348c
raze. <over404lord@gmail.com>
2025-06-29 13:45:39 +0500

    authentication /user/profile

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -2,9 +2,21 @@
 import Aside from '@/src/components/Aside/Aside'
 import ButtonDefault from '@/src/components/Button/ButtonDefault'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import AlertDefault from '@/src/components/Alert/AlertDefault';
 
 export default function UserLayout({ children }: { children: ReactNode }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            AlertDefault.error("Avval identifikatsiyadan o'ting!")
+            router.push("/login")
+        }
+    }, [router])
+
     return (
         <div className="flex min-h-screen">
             <Aside />
@@ -17,7 +29,9 @@ export default function UserLayout({ children }: { children: ReactNode }) {
                         customClasses='bg-transparent !text-black tracking-[1px] text-xl mb-5 hover:bg-transparent !px-0 !py-0 ml-2'
                     />
                 </div>
-                <main className="h-full border-1 rounded-2xl bg-light-gray border-gray-200 shadow-[4px_0_6px_-1px_rgba(0,0,0,0.1)] p-10">{children}</main>
+                <main className="h-full border-1 rounded-2xl bg-light-gray border-gray-200 shadow-[4px_0_6px_-1px_rgba(0,0,0,0.1)] p-10">
+                    {children}
+                </main>
             </div>
         </div>
     )
