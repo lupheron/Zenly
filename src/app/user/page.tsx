b3975a86488e208509420527977c413c2882387b
raze. <over404lord@gmail.com>
2025-07-03 21:19:55 +0500

    type checking login

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,4 +1,25 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useUser } from '@/src/hooks/users/useUser'
+
 export default function Dashboard() {
+    const router = useRouter()
+    const { data, isLoading } = useUser()
+
+    useEffect(() => {
+        if (!isLoading && data) {
+            if (data.type !== 0) {
+                router.push('/')
+            }
+        }
+    }, [data, isLoading, router])
+
+    if (isLoading || !data) {
+        return <p>Yuklanmoqda...</p>
+    }
+
     return (
         <div>
             <h1 className="text-2xl font-bold">Dashboard</h1>
