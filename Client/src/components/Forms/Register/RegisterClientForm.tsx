c2b50860a16c1521c2018bb890f150f8468dbf87
raze. <over404lord@gmail.com>
2025-07-21 17:58:20 +0500

    not found page

--- Client/src/components/Forms/Register/RegisterClientForm.tsx
+++ Client/src/components/Forms/Register/RegisterClientForm.tsx
@@ -163,7 +163,7 @@ const RegisterClientForm = () => {
             <button
                 type="submit"
                 disabled={isPending}
-                className="w-full bg-light-green text-white px-4 sm:px-6 py-2 sm:py-3 rounded-md text-sm sm:text-base font-semibold hover:bg-green-600 transition-colors duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed mt-6"
+                className="w-full cursor-pointer bg-light-green text-white px-4 sm:px-6 py-2 sm:py-3 rounded-md text-sm sm:text-base font-semibold hover:bg-green-600 transition-colors duration-200 disabled:bg-gray-400 disabled:cursor-not-allowed mt-6"
             >
                 {isPending ? "Yuborilmoqda..." : "Ro'yxatdan o'tish"}
             </button>
